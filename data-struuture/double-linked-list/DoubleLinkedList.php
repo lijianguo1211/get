347c3382f77eb4358a17131a592bfda1b26d8f9b
Jay.Li <jay.li@feisu.com>
2019-09-09 19:56:05 +0800

    添加双链表

--- data-struuture/double-linked-list/DoubleLinkedList.php
+++ data-struuture/double-linked-list/DoubleLinkedList.php
@@ -25,9 +25,42 @@ class DoubleLinkedList
             $temp = $temp->next;//指针后移
         }
 
-        $temp->next = $headNote;
-        $headNote->prev = $temp;
+        $temp->next = $headNote;//临时节点的next指针指向待插入节点
+        $headNote->prev = $temp;//待插入节点的prev指针指向临时节点
+    }
 
+    public function update(HeadNote $headNote)
+    {
+
+    }
+
+    public function del(int $id)
+    {
+        //临时指针，初始指向头节点
+        $temp = $this->head;
+        //记录状态的变量，初始为false
+        $flag = false;
+
+        while (true) {
+            if ($temp->next === null) {
+                break;//代表链表遍历结束
+            }
+
+            if ($temp->next->id === $id) {
+                $flag = true;
+                break;//找到了链表中节点ID
+            }
+            //指针后移
+            $temp = $temp->next;
+        }
+
+        if ($flag) {
+            //改变指针next和prev的指向
+            $temp->next = $temp->next->next;
+            $temp->next->next->prev = $temp;
+        } else {
+            return printf("链表中不存在节点ID为：$id 的节点");
+        }
     }
 
     public function list()
@@ -67,4 +100,13 @@ $obj->add($test3);
 $obj->add($test4);
 $obj->add($test5);
 
+$obj->list();
+echo "\n";
+$obj->del(3);
+$obj->list();
+echo "\n";
+$obj->del(1);
+$obj->list();
+echo "\n";
+$obj->del(5);
 $obj->list();
