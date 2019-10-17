7dfec2135370e53bd0cfeb11fe7abeaf57b2fefb
lijianguo1211 <risesli@hotmail.com>
2019-10-17 21:32:04 +0800

    change

--- index.php
+++ index.php
@@ -25,22 +25,4 @@ $arr = [];
 var_dump($arr[1]);
 
 
-function index()
-{
-    /**
-     * 这个就是你的业务层处理逻辑
-     */
-    $mysql = new PDO('', '', '', '');
-    if (true) {
-        $userName = md5(123456);
-    } else {
-        $userName = '';
-    }
-    /**
-     * 这个就是你的界面显示的html.等
-     */
-    $html = <<<ERT
-<input name="user" value="{$userName}">//$userName就是你业务处理传递进来的变量
-ERT;
-    echo $html;
-}
+//* * * * * cd //data/apps/lijianguo && php artisan schedule:run >> /dev/null 2>&1
