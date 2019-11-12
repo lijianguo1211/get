542c16b63c49ad4e6e87f6b2e5e3dadcf512025b
Jay.Li <jay.li@feisu.com>
2019-11-12 14:39:22 +0800

    希尔排序

--- data-struuture/sort-algorithm/ShellSort.php
+++ data-struuture/sort-algorithm/ShellSort.php
@@ -3,6 +3,14 @@
 
 class ShellSort
 {
+    /**
+     * Notes: 希尔排序之交换法排序
+     * User: LiYi
+     * Date: 2019/11/12 0012
+     * Time: 14:30
+     * @param array $data
+     * @return array
+     */
     public static function shellSortArray(array $data):array
     {
         if (!is_array($data)) {
@@ -48,22 +56,33 @@ class ShellSort
         return [$data, $count];
     }
 
+    /**
+     * Notes: 希尔排序之移位法排序
+     * User: LiYi
+     * Date: 2019/11/12 0012
+     * Time: 14:29
+     * @param array $data
+     * @return array
+     */
     public static function ShellSortMoveArray(array $data)
     {
-        $count = count($data);
-        for ($gap = $count / 2; $gap >= 1; $gap /= 2) {
+        $count = count($data);//得到数组总数
+        for ($gap = $count / 2; $gap > 0; $gap /= 2) {//缩小增量，每次减半
             $gap = floor($gap);
-            for ($i = $gap; $i < $count; $i++) {
-                $insertIndex = $i;
-                $insertValue = $data[$insertIndex];
+            for ($i = $gap; $i < $count; $i++) {//
+                $insertIndex = $i;//待插入元素的下表
+                $insertValue = $data[$insertIndex];//待插入元素的值
                 echo "insertIndex=$insertIndex" . PHP_EOL;
                 echo "insertValue=$insertValue" . PHP_EOL;
-                if ($data[$insertIndex] < $data[$insertIndex - $gap]) {
-                    while ($insertIndex - $gap > 0 && $insertValue < $data[$insertIndex - $gap]) {
+                if ($data[$insertIndex] < $data[$insertIndex - $gap]) {//判断待插入元素和它步长的元素比较，待插入元素小就进入循环
+                    //判断是否越界了，第一个元素的下标是要大于等于0的，否则退出循环
+                    //判断后面的元素比前面的元素小，进入循环，否则退出循环
+                    while ($insertIndex - $gap >= 0 && $insertValue < $data[$insertIndex - $gap]) {
+                        //把步长前面的大的值向后移动
                         $data[$insertIndex] = $data[$insertIndex - $gap];
-                        $insertIndex -= $gap;
+                        $insertIndex -= $gap;//每循环一次就把带插入的坐标减去补偿
                     }
-
+                    //把带插的小值插入到前面
                     $data[$insertIndex] = $insertValue;
                 }
             }
@@ -109,4 +128,31 @@ class ShellSort
         var_dump($data);
     }
 }
-var_dump(ShellSort::shellSortMoveArray([9,6,1,3,0,5,7,2,8,4]));
+var_dump(ShellSort::shellSortMoveArray([0 => 9, 1 => 6, 2 => 1, 3 => 3, 4 => 0, 5 => 5, 6 => 7, 7 => 2, 8 => 8, 9 => 4]));
+
+// $gap = 10 / 2 = 5
+
+// $insertIndex  = $i = $gap = 5
+// $insertValue = $data[$insertIndex] = $data[5] = 5;
+// $data[$insertIndex] < $data[$insertIndex - $gap] == $data[5] < $data[5-5] = $data[0] ==> 5 < 9
+// while(5 - 5 >= 0 && 5 < 9) {
+//  $data[5] = $data[5-5] = $data[0] = 9
+//  $insertIndex -= 5 = 0;
+//}
+// $data[$insertIndex] = $data[0] = $insertValue = 5
+// $i++ = 6;
+// $insertIndex  = $i =  6
+// $insertValue = $data[$insertIndex] = $data[6] = 7;
+// $data[$insertIndex] < $data[$insertIndex - $gap] == $data[6] < $data[6-5] = $data[1] ==> 7 < 6
+// $i++ = 7;
+// $insertIndex  = $i =  7
+// $insertValue = $data[$insertIndex] = $data[7] = 2;
+// $data[$insertIndex] < $data[$insertIndex - $gap] == $data[7] < $data[7-5] = $data[2] ==> 2 < 1
+// $i++ = 8;
+// $insertIndex  = $i =  8
+// $insertValue = $data[$insertIndex] = $data[8] = 8;
+// $data[$insertIndex] < $data[$insertIndex - $gap] == $data[8] < $data[8-5] = $data[3] ==> 8 < 3
+// $i++ = 9;
+// $insertIndex  = $i =  9
+// $insertValue = $data[$insertIndex] = $data[9] = 4;
+// $data[$insertIndex] < $data[$insertIndex - $gap] == $data[9] < $data[9-5] = $data[4] ==> 4 < 0
